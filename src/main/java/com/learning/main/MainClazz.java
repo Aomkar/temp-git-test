dd71eb8366996233259c288b0da2fcfb7be74fc5
Aomkar Mathakar <amathakar@nvidia.com>
2025-06-28 14:24:23 -0700

    Some println commit

--- src/main/java/com/learning/main/MainClazz.java
+++ src/main/java/com/learning/main/MainClazz.java
@@ -4,7 +4,7 @@ public class MainClazz {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World!");
+		System.out.println("Testing jgit for commit log traversal");
 	}
 
 }
