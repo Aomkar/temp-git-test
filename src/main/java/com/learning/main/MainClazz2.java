a33118a1670a581a4df5207756f104ef73f47c56
Aomkar Mathakar <amathakar@nvidia.com>
2025-06-30 09:17:18 -0700

    Another commit

---
+++ src/main/java/com/learning/main/MainClazz2.java
@@ -0,0 +1,9 @@
+package com.learning.main;
+
+public class MainClazz2 {
+
+	public static void main(String[] args) {
+		System.out.println("Some temp message to increase commits... to test JGit for reading commit logs...");
+	}
+
+}
